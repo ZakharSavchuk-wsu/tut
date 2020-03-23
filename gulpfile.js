89d32f03ab6bd1449f32916bad8fb0ef44068e1f
Zakhar_Savchuk <savchukzahar33@gmail.com>
2020-03-23 19:47:29 +0200

    edit gulpfile and configure auto reload on changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,41 @@
 var gulp = require('gulp');
-
 var sass = require('gulp-sass');
+var soucemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
 
-function convert(done){
-gulp.src('./scss/style.scss')
-.pipe(sass({
-    errorLogToConsole: true
-})).on('error', console.error.bind(console))
-.pipe(gulp.dest('./css/'));
+function convert(done) {
+    gulp.src('./scss/**/*.scss')
+        .pipe(soucemaps.init())
+        .pipe(sass({
+            errorLogToConsole: true
+        }))
+        .on('error', console.error.bind(console))
+        .pipe(soucemaps.write('./'))
+        .pipe(gulp.dest('./css/'))
+        .pipe(browserSync.stream());
+    done();
+}
+function Sync(done){
+    browserSync.init({
+        server:{
+            baseDir: './'
+        },
+        port: 3000
+    });
     done();
 }
 
-gulp.task(convert);
+function browserReload(done){
+    browserSync.reload();
+    done();
+}
+
+function watchFiles(){
+    gulp.watch("./scss/**/*", convert);
+    gulp.watch("./**/*.html", browserReload);
+    gulp.watch("./**/*.js", browserReload);
+
+}
+
+// gulp.task(convert);
+gulp.task('default', gulp.parallel(Sync, watchFiles));
